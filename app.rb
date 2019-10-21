387c160fa45ba2daeb430d3ec29d915be5c2ef96
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 18:29:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Jessica"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Colonial Heights, VA"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is TOO MANY TO CHOOSE FROM!"
+  end 
+  
 end
